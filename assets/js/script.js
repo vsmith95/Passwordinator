53ecd183acd35cc7c42540c94bccc8db382a41a2
vsmith95 <vaughnsmith95@gmail.com>
2022-02-26 07:34:49 -0500

    created the section of the function that will create needed variables for password generation, as well as looping the function if an unusable value is entered

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,53 @@
 // Assignment code here
+function generatePassword() {
+  // prompt asking for how many characters password will contain, needs logic to only accept numeric values
+    var passwordLength = window.prompt("Enter length of password (numbers only)");
+    // loops function if value entered is not an integer
+    if ( /^[0-9]+$/.test(passwordLength)) {
+      console.log(passwordLength);
+    
+    } 
 
+    else {
+      window.confirm("This is not a valid entry! Please start over!");
+      generatePassword();
+      return;
+    }
+  // checks true or false for if the password will contain lowercase letters
+  var isLowerCase = window.confirm("Does this contain lowercase letters?");
+  console.log(isLowerCase);
+
+  // checks true or false for if the password will contain uppercase letters
+  var isUpperCase = window.confirm("Does this contain uppercase letters?");
+  console.log(isUpperCase);
+
+  // checks true or false for if the password will contain special characters
+  var isSpecial = window.confirm("Does this contain special characters?");
+  console.log(isSpecial);
+
+  // asks user for the amount of special characters if applicable, otherwise sets the value to 0
+  if (isSpecial === true) {
+    
+    var specialCount = window.prompt("How many special characters?");
+  }
+
+  else {
+    specialCount = "0";
+  }
+
+  // loops function if value entered is not an integer
+  if ( /^[0-9]+$/.test(specialCount)) {
+    console.log(specialCount);
+  
+  } 
+
+  else {
+    window.confirm("This is not a valid entry! Please start over!");
+    generatePassword();
+    return;
+  }
+ 
+}
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
