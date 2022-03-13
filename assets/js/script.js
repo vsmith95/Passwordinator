27dca7ffc0cd8cabb02f2f17f7579daea18d08c8
vsmith95 <vaughnsmith95@gmail.com>
2022-03-13 17:15:23 -0400

    honestly just kinda stuck

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,15 @@
 // Assignment code here
+var upperCaseCodes = charCodes(65, 90)
+var lowerCaseCodes = charCodes(97, 122)
+var numberCharCodes = charCodes(48, 57)
+var specialCharCodes = charCodes(33, 47).concat(
+  charCodes(58, 64)
+).concat(
+  charCodes(91, 96)
+).concat(
+  charCodes(123, 126)
+)
+
 function generatePassword() {
   // prompt asking for how many characters password will contain, needs logic to only accept numeric values
     var passwordLength = window.prompt("Enter length of password (numbers only)");
@@ -21,32 +32,40 @@ function generatePassword() {
   var isUpperCase = window.confirm("Does this contain uppercase letters?");
   console.log(isUpperCase);
 
+   // checks true or false for if the password will contain numbers
+   var isNumber = window.confirm("Does this contain any numbers?");
+   console.log(isNumber);
+
   // checks true or false for if the password will contain special characters
   var isSpecial = window.confirm("Does this contain special characters?");
   console.log(isSpecial);
-
-  // asks user for the amount of special characters if applicable, otherwise sets the value to 0
-  if (isSpecial === true) {
-    
-    var specialCount = window.prompt("How many special characters?");
-  }
-
-  else {
-    specialCount = "0";
-  }
-
-  // loops function if value entered is not an integer
-  if ( /^[0-9]+$/.test(specialCount)) {
-    console.log(specialCount);
-  
-  } 
-
-  else {
-    window.confirm("This is not a valid entry! Please start over!");
-    generatePassword();
-    return;
-  }
  
+  // Generate the password based on above variables
+  function password(passwordLength, isLowerCase, isUpperCase, isNumber, isSpecial) {
+    var charCodes = lowerCaseCodes
+    if (isLowerCase < isUpperCase){
+      charCodes = upperCaseCodes
+    } else {
+      charCodes = lowerCaseCodes
+    }
+    if (isUpperCase = isLowerCase) charCodes = charCodes.concat(upperCaseCodes)
+    if (isSpecial) charCodes = charCodes.concat(specialCharCodes)
+    if (isNumber) charCodes = charCodes.concat(numberCharCodes)
+
+    for (i = o, i < passwordLength; i++;) {
+      var character = charCodes[Math.floor(Math.random() * passwordLength)]
+      password.push(String.fromCharCode(character))  
+    }
+    return password.join()
+  
+  function charCodes(low, high) {
+    var array = []
+    for (let i = low; i <= high; i++) {
+      array.push(i)
+    }
+    return array
+  }
+}
 }
 
 // Get references to the #generate element
@@ -57,8 +76,7 @@ function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
-  passwordText.value = password;
-
+  passwordText.innerText = password;
 }
 
 // Add event listener to generate button
